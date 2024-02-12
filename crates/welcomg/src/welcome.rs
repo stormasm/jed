435218044cbf6df48e4bfd5a9eba64659eeb8b5a
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-02-12 11:17:07 -0800

    more pare down of welcomg

--- crates/welcomg/src/welcome.rs
+++ crates/welcomg/src/welcome.rs
@@ -1,30 +1,19 @@
-//mod base_keymap_picker;
-//mod base_keymap_setting;
-
-//use client::{telemetry::Telemetry, TelemetrySettings};
 use db::kvp::KEY_VALUE_STORE;
 use gpui::{
     svg, AnyElement, AppContext, EventEmitter, FocusHandle, FocusableView, InteractiveElement,
-    ParentElement, Render, Styled, Subscription, View, ViewContext, VisualContext, WeakView,
-    WindowContext,
+    ParentElement, Render, Styled, View, ViewContext, VisualContext, WeakView, WindowContext,
 };
-use settings::SettingsStore;
 use std::sync::Arc;
 use ui::prelude::*;
-//use vim::VimModeSetting;
 use workspace::{
     dock::DockPosition,
     item::{Item, ItemEvent},
     open_new, AppState, Welcomg, Workspace, WorkspaceId,
 };
 
-//pub use base_keymap_setting::BaseKeymap;
-
 pub const FIRST_OPEN: &str = "first_open";
 
 pub fn init(cx: &mut AppContext) {
-    // BaseKeymap::register(cx);
-
     cx.observe_new_views(|workspace: &mut Workspace, _cx| {
         workspace.register_action(|workspace, _: &Welcomg, cx| {
             let welcome_page = WelcomePage::new(workspace, cx);
@@ -32,8 +21,6 @@ pub fn init(cx: &mut AppContext) {
         });
     })
     .detach();
-
-    //base_keymap_picker::init(cx);
 }
 
 pub fn show_welcome_view(app_state: &Arc<AppState>, cx: &mut AppContext) {
@@ -54,8 +41,6 @@ pub fn show_welcome_view(app_state: &Arc<AppState>, cx: &mut AppContext) {
 pub struct WelcomePage {
     workspace: WeakView<Workspace>,
     focus_handle: FocusHandle,
-    //telemetry: Arc<Telemetry>,
-    _settings_subscription: Subscription,
 }
 
 impl Render for WelcomePage {
@@ -77,16 +62,10 @@ impl Render for WelcomePage {
                             .h(px(96.))
                             .mx_auto(),
                     )
-                    .child(h_flex().justify_center().child(Label::new("Hello World")))
                     .child(
-                        v_flex()
-                            .p_3()
-                            .gap_2()
-                            .bg(cx.theme().colors().elevated_surface_background)
-                            .border_1()
-                            .border_color(cx.theme().colors().border)
-                            .rounded_md()
-                            .child(h_flex().gap_2().child(Label::new("Send crash reports"))),
+                        h_flex()
+                            .justify_center()
+                            .child(Label::new("Hello to the Welcomg World")),
                     ),
             )
     }
@@ -94,48 +73,12 @@ impl Render for WelcomePage {
 
 impl WelcomePage {
     pub fn new(workspace: &Workspace, cx: &mut ViewContext<Workspace>) -> View<Self> {
-        let this = cx.new_view(|cx| {
-            /*
-                        cx.on_release(|this: &mut Self, _, _| {
-                            this.telemetry
-                                .report_app_event("welcome page: close".to_string());
-                        })
-                        .detach();
-            */
-            WelcomePage {
-                focus_handle: cx.focus_handle(),
-                workspace: workspace.weak_handle(),
-                //telemetry: workspace.client().telemetry().clone(),
-                _settings_subscription: cx
-                    .observe_global::<SettingsStore>(move |_, cx| cx.notify()),
-            }
+        let this = cx.new_view(|cx| WelcomePage {
+            focus_handle: cx.focus_handle(),
+            workspace: workspace.weak_handle(),
         });
-
         this
     }
-
-    /*
-    fn update_settings<T: Settings>(
-        &mut self,
-        selection: &Selection,
-        cx: &mut ViewContext<Self>,
-        callback: impl 'static + Send + Fn(&mut T::FileContent, bool),
-    ) {
-        if let Some(workspace) = self.workspace.upgrade() {
-            let fs = workspace.read(cx).app_state().fs.clone();
-            let selection = *selection;
-            settings::update_settings_file::<T>(fs, cx, move |settings| {
-                let value = match selection {
-                    Selection::Unselected => false,
-                    Selection::Selected => true,
-                    _ => return,
-                };
-
-                callback(settings, value)
-            });
-        }
-    }
-    */
 }
 
 impl EventEmitter<ItemEvent> for WelcomePage {}
@@ -176,7 +119,7 @@ impl Item for WelcomePage {
             focus_handle: cx.focus_handle(),
             workspace: self.workspace.clone(),
             //telemetry: self.telemetry.clone(),
-            _settings_subscription: cx.observe_global::<SettingsStore>(move |_, cx| cx.notify()),
+            //_settings_subscription: cx.observe_global::<SettingsStore>(move |_, cx| cx.notify()),
         }))
     }
 
